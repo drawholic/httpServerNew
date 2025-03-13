3866ee37f619540b8b7d40f648cefc502d2c8f53
drawholic <mamrokhavlad@gmail.com>
2025-03-13 20:18:50 +0200

    bind_socket def, fill_address and create_socket return type changed to int, added return statements

--- src/Server.cpp
+++ src/Server.cpp
@@ -13,27 +13,37 @@ void Server::run(){};
 
 
 namespace server_setup{
-	void create_socket(int sock)
+	int create_socket(int sock)
 	{
 		sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 		if(sock == -1)
 		{
 			perror("Failure creating socket");
+			return -1;
 		};
+		return 0;
 	};
 
-	void fill_address(sockaddr_in* addr, const char* ip, int port)
+	int fill_address(sockaddr_in* addr, const char* ip, int port)
 	{
 		if(inet_aton(ip, &addr->sin_addr) == 0)
 		{
 			printf("Failure setting address to string: %s\n", ip);
+			return -1;
 		};
 		addr->sin_port = port;
 		addr->sin_family = AF_INET;
+		return 0;
 	};
 
-	void bind_socket(int sock, sockaddr_in* addr){
-
+	int bind_socket(int sock, sockaddr_in* addr){
+		int status = bind(sock, (sockaddr*)addr, sizeof(*addr));
+		if(status == -1)
+		{
+			perror("Failure on binding");
+			return -1;
+		};
+		return 0;
 	};
 
 
