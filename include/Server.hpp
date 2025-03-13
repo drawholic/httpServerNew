a02717cfa0f8d8cd0091b8bd61075a1077a10a8b
drawholic <mamrokhavlad@gmail.com>
2025-03-13 19:37:04 +0200

    namespace for server_setup added, address, address length and init_server() declare

--- include/Server.hpp
+++ include/Server.hpp
@@ -5,6 +5,21 @@
 #include "ReadWrite.hpp"
 #include "ClientsContainer.hpp"
 
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+namespace server_setup{
+	
+	void create_socket(int);
+
+	void fill_address(sockaddr_in*, const char*, int);
+
+	void bind_socket(int, sockaddr_in*);
+
+	void socket_listen(int);
+
+	void set_options(int);
+};
 
 
 class Server
@@ -13,6 +28,11 @@ class Server
 	ReadWrite* rw;
 	ClientsContainer* fds;
 	char* buffer;
+
+	sockaddr_in* addr;
+	socklen_t addrlen;
+
+	void init_server();
 
 public:
 	Server(const char* ip = 0, int port = 0);
