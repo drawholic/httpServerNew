05a113a7cef01acbe4ca827da98de28621a06e9c
drawholic <mamrokhavlad@gmail.com>
2025-03-13 20:59:52 +0200

    find_fd def, empty add and constructor

--- src/ClientsContainer.cpp
+++ src/ClientsContainer.cpp
@@ -1,1 +1,23 @@
 #include "ClientsContainer.hpp"
+
+
+ClientsContainer::ClientsContainer()
+{
+
+};
+
+
+
+int ClientsContainer::add(int client_fd)
+{
+
+};
+
+pollfd_it ClientsContainer::find_fd(int fd)
+{
+	pollfd_it it = std::find_if(fds.begin(), fds.end(), [&fd](pollfd el){
+		return el.fd == fd;
+	});
+
+	return it;
+};
