5e29ba3853df558f0173280180c8126bf07fa0fd
drawholic <mamrokhavlad@gmail.com>
2025-03-13 21:02:44 +0200

    def for check_if_exists and add

--- src/ClientsContainer.cpp
+++ src/ClientsContainer.cpp
@@ -10,6 +10,19 @@ ClientsContainer::ClientsContainer()
 
 int ClientsContainer::add(int client_fd)
 {
+	if(check_if_exists(fd))
+	{
+		printf("Already exists");
+		return -1;
+	}else
+	{
+		pollfd new_fd;
+		new_fd.fd = client_fd;
+		new_fd.event = POLLIN;
+
+		fds.push_back(new_fd);
+		return 1;
+	};
 
 };
 
@@ -20,4 +33,9 @@ pollfd_it ClientsContainer::find_fd(int fd)
 	});
 
 	return it;
+};
+
+bool ClientsContainer::check_if_exists(int fd)
+{
+	return fds.end() == find_fd(fd);
 };
