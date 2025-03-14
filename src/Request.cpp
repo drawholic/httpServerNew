c0edc5f518a454b214f6e1dae2ae6d4156bb79e7
drawholic <mamrokhavlad@gmail.com>
2025-03-14 16:46:20 +0200

    fill_members, get_method def. redef startline_regex and headers_regex as regex

--- src/Request.cpp
+++ src/Request.cpp
@@ -1,9 +1,40 @@
 #include "Request.hpp"
 
-std::string startline_regex = "(.+) (.+) (.+)";
-std::string headers_regex = "(.+): (.+)";
+std::regex startline_regex = "(.+) (.+) (.+)";
+std::regex headers_regex = "(.+): (.+)";
 
-void RequestStartLine::fill_members(){
+MethodsEnum get_method(std::string& string_method)
+{
+	if(string_method == "GET")
+		return GET;
+	
+	if(string_method == "POST")
+		return POST;
 
+	if(string_method == "DELETE")
+		return DELETE;	
+
+	if(string_method == "HEAD")
+		return HEAD;
+	
+	if(string_method == "PATCH")
+		return PATCH;
+	
+	if(string_method == "PUT")
+		return PUT;
+	return NONE;
+
+};
+
+void RequestStartLine::fill_members(std::string& input){
+
+	std::smatch cm;
+	std::regex_match(input, cm, startline_regex);
+
+	method = get_method(cm[0]);
+
+	uri = cm[1];
+
+	version = cm[2];
 
 };
