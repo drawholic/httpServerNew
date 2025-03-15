72e7df7866317c067702d8b32dfb6c7b7a252133
drawholic <mamrokhavlad@gmail.com>
2025-03-15 12:41:42 +0200

    parse def, empty constructor

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,1 +1,33 @@
 #include "Parser.hpp"
+
+Parser::Parser()
+{
+
+};
+
+
+Request Parser::parse(std::string input)
+{
+	Request rq;
+	std::string startline;
+	std::string headers;
+	std::string body;
+
+	unsigned end_of_startline;
+	unsigned end_of_headers;
+	
+	end_of_startline = input.find('\n');
+	end_of_headers = input.find("\n\n");
+
+	startline = input.substr(0, end_of_startline);
+	headers = input.substr(end_of_startline+1, end_of_headers - end_of_startline);
+ 
+
+	rq.fill_startline(startline);
+	rq.fill_headers(headers);
+	rq.body = input.substr(end_of_headers);
+
+	rq.print();
+
+	return rq;
+};
